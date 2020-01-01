d1220c791c4f068ce16ef261e0799e5695a56bd9
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-01-01 09:17:35 +0200

    change in orders migration file

--- database/migrations/2019_12_31_072847_create_orders_table.php
+++ database/migrations/2019_12_31_072847_create_orders_table.php
@@ -15,8 +15,8 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('orderID');
-            $table->integer('buyer');
-            $table->integer('product');
+            $table->integer('buyerID');
+            $table->integer('productID');
             $table->timestamps();
         });
     }
